168a63d4464b4b303ab2598a47c68b84217249fe
dorodango-maker <tippy.gfop@gmail.com>
2024-05-27 10:47:04 +0900

    refactor: 一部処理を抽象化

--- main.go
+++ main.go
@@ -10,18 +10,26 @@ import (
     "github.com/hajimehoshi/ebiten/v2/vector"
 )
 
+type Symbol int
+
+const (
+    Empty Symbol = iota
+    X
+    O
+)
+
 type Position struct {
     Row, Col int
 }
 
 type Game struct {
-    board [3][3]int // 0: 空, 1: X, 2: O
-    turn int        // 1: Xのターン, 2: Oのターン
+    board [3][3]Symbol
+    turn Symbol
     xImg *ebiten.Image
     oImg *ebiten.Image
     xImgTransparent *ebiten.Image
     oImgTransparent *ebiten.Image
-    winner int       // 0: まだ勝者なし, 1: Xの勝利, 2: Oの勝利
+    winner Symbol
     xPositions []Position
     oPositions []Position
     oldestPosition Position
@@ -33,7 +41,6 @@ func loadAndResizeImage(filePath string, width, height int) (*ebiten.Image, erro
         return nil, err
     }
 
-    // 画像をリサイズ
     resizedImg := ebiten.NewImage(width, height)
     op := &ebiten.DrawImageOptions{}
     op.GeoM.Scale(float64(width)/float64(img.Bounds().Dx()), float64(height)/float64(img.Bounds().Dy()))
@@ -66,13 +73,13 @@ func NewGame() *Game {
     }
 
     return &Game{
-        board: [3][3]int{},
-        turn: 1,
+        board: [3][3]Symbol{},
+        turn: X,
         xImg: xImg,
         oImg: oImg,
         xImgTransparent: xImgTransparent,
         oImgTransparent: oImgTransparent,
-        winner: 0,
+        winner: Empty,
         xPositions: []Position{},
         oPositions: []Position{},
         oldestPosition: Position{Row: -1, Col: -1},
@@ -80,94 +87,94 @@ func NewGame() *Game {
 }
 
 func (g *Game) resetGame() {
-    g.board = [3][3]int{}
-    g.turn = 1
-    g.winner = 0
+    g.board = [3][3]Symbol{}
+    g.turn = X
+    g.winner = Empty
     g.xPositions = []Position{}
     g.oPositions = []Position{}
     g.oldestPosition = Position{Row: -1, Col: -1}
 }
 
-func checkWin(board [3][3]int) int {
-    // 横のチェック
+func checkWin(board [3][3]Symbol) Symbol {
     for i := 0; i < 3; i++ {
-        if board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+        if board[i][0] != Empty && board[i][0] == board[i][1] && board[i][1] == board[i][2] {
             return board[i][0]
         }
     }
-    // 縦のチェック
+
     for i := 0; i < 3; i++ {
-        if board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i] {
+        if board[0][i] != Empty && board[0][i] == board[1][i] && board[1][i] == board[2][i] {
             return board[0][i]
         }
     }
-    // 斜めのチェック
-    if board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+
+    if board[0][0] != Empty && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
         return board[0][0]
     }
-    if board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+    if board[0][2] != Empty && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
         return board[0][2]
     }
 
-    return 0
+    return Empty
 }
 
-func (g *Game) addMark(row, col int) {
-    pos := Position{Row: row, Col: col}
-    if g.turn == 1 {
-        if len(g.xPositions) == 3 {
-            oldest := g.xPositions[0]
-            g.board[oldest.Row][oldest.Col] = 0
-            g.xPositions = g.xPositions[1:]
-            // 古い位置が消えた場合、最も古い位置を更新
-            g.oldestPosition = pos
-        }
-        g.xPositions = append(g.xPositions, pos)
+func (g *Game) addMark(pos Position) {
+    if g.turn == X {
+        g.updatePositions(&g.xPositions, pos)
     } else {
-        if len(g.oPositions) == 3 {
-            oldest := g.oPositions[0]
-            g.board[oldest.Row][oldest.Col] = 0
-            g.oPositions = g.oPositions[1:]
-            g.oldestPosition = pos
-        }
-        g.oPositions = append(g.oPositions, pos)
+        g.updatePositions(&g.oPositions, pos)
     }
-    g.board[row][col] = g.turn
+    g.board[pos.Row][pos.Col] = g.turn
+}
+
+func (g *Game) updatePositions(positions *[]Position, pos Position) {
+    if len(*positions) == 3 {
+        oldest := (*positions)[0]
+        g.board[oldest.Row][oldest.Col] = Empty
+        *positions = (*positions)[1:]
+        g.oldestPosition = pos
+    }
+    *positions = append(*positions, pos)
 }
 
 func (g *Game) Update() error {
-    if g.winner != 0 {
-        // 勝者決定後のみリセットボタンのクリックを検出
+    if g.winner != Empty {
         if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
             g.resetGame()
         }
         return nil
     }
 
-    // 勝利判定
     g.winner = checkWin(g.board)
-    if g.winner != 0 {
+    if g.winner != Empty {
         log.Printf("Player %d wins!", g.winner)
         return nil
     }
 
-    // クリック位置に応じて、記号を配置
     if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
         x, y := ebiten.CursorPosition()
         row := y / 100
         col := x / 100
-        // 古い位置には置けないようにする
-        if row < 3 && col < 3 && g.board[row][col] == 0 && !(g.oldestPosition.Row == row && g.oldestPosition.Col == col) {
-            g.addMark(row, col)
-            if g.turn == 1 {
-                g.turn = 2
-            } else {
-                g.turn = 1
-            }
+        pos := Position{Row: row, Col: col}
+        if g.isValidMove(pos) {
+            g.addMark(pos)
+            g.toggleTurn()
         }
     }
 
     return nil
+}
+
+func (g *Game) isValidMove(pos Position) bool {
+    return pos.Row < 3 && pos.Col < 3 && g.board[pos.Row][pos.Col] == Empty && !(g.oldestPosition == pos)
+}
+
+func (g *Game) toggleTurn() {
+    if g.turn == X {
+        g.turn = O
+    } else {
+        g.turn = X
+    }
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -175,45 +182,47 @@ func (g *Game) Draw(screen *ebiten.Image) {
     const cellSize = 100
     const iconSize = 75
 
-    // 背景を黒で塗りつぶす
     screen.Fill(color.RGBA{0, 0, 0, 255})
 
-    // グリッドを白で描画
     for i := 1; i < gridSize; i++ {
         vector.StrokeLine(screen, float32(i*cellSize), 0, float32(i*cellSize), float32(gridSize*cellSize), 2, color.RGBA{255, 255, 255, 255}, false)
         vector.StrokeLine(screen, 0, float32(i*cellSize), float32(gridSize*cellSize), float32(i*cellSize), 2, color.RGBA{255, 255, 255, 255}, false)
     }
 
-    // ボードに記号を描画
     for row := 0; row < 3; row++ {
         for col := 0; col < 3; col++ {
             op := &ebiten.DrawImageOptions{}
             op.GeoM.Translate(float64(col*cellSize+(cellSize-iconSize)/2), float64(row*cellSize+(cellSize-iconSize)/2))
-            if g.board[row][col] == 1 {
+            if g.board[row][col] == X {
                 screen.DrawImage(g.xImg, op)
-            } else if g.board[row][col] == 2 {
+            } else if g.board[row][col] == O {
                 screen.DrawImage(g.oImg, op)
             }
         }
     }
 
-    // 古い記号を半透明な画像で描画
-    if g.turn == 1 && len(g.xPositions) == 3 {
-        oldest := g.xPositions[0]
-        g.board[oldest.Row][oldest.Col] = 0 // 元の記号を消す
-        drawTransparentMark(screen, oldest.Row, oldest.Col, g.xImgTransparent)
-        g.oldestPosition = oldest
-    } else if g.turn == 2 && len(g.oPositions) == 3 {
-        oldest := g.oPositions[0]
-        g.board[oldest.Row][oldest.Col] = 0 // 元の記号を消す
-        drawTransparentMark(screen, oldest.Row, oldest.Col, g.oImgTransparent)
-        g.oldestPosition = oldest
-    }
+    g.drawOldestMark(screen)
 
-    // 勝者のメッセージを表示
-    if g.winner != 0 {
+    if g.winner != Empty {
         msg := fmt.Sprintf("Player %d wins! Right-click to reset.", g.winner)
         ebitenutil.DebugPrint(screen, msg)
+    }
+}
+
+func (g *Game) drawOldestMark(screen *ebiten.Image) {
+    if (g.turn == X && len(g.xPositions) == 3) || (g.turn == O && len(g.oPositions) == 3) {
+        var oldest Position
+        var img *ebiten.Image
+        if g.turn == X {
+            oldest = g.xPositions[0]
+            img = g.xImgTransparent
+        } else {
+            oldest = g.oPositions[0]
+            img = g.oImgTransparent
+        }
+        g.board[oldest.Row][oldest.Col] = Empty
+        drawTransparentMark(screen, oldest.Row, oldest.Col, img)
+        g.oldestPosition = oldest
     }
 }
 
